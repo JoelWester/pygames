d65b290f6784a8bb2b854cd071cdf86919214e87
Joel <44907762+JoelWester@users.noreply.github.com>
2022-02-04 15:07:05 +0200

    Add files via upload
    SpaceGame ship Class changed to Body Class of which ship is an object

--- SpaceGame.py
+++ SpaceGame.py
@@ -15,18 +15,22 @@ centre = (width//2,height//2)
 show_axes = False
 
 #Gravitational constant
-g = 0.0002
-#Ship object
+g = 0.0001
+#Body object
 
-class Ship:
+class Body:
     position =(50.0, 50.0)
     mass = 0.5
     velocity = (0.0, 0.0)
+    def __init__(self, type):
+        self.type = type
 
-print('Ship details:')
-print('pos: ', Ship.position)
-print('mass: ', Ship.mass)
-print('velocity: ', Ship.velocity)
+ship = Body("Ship")
+
+print('Body details:')
+print('pos: ', ship.position)
+print('mass: ', ship.mass)
+print('velocity: ', ship.velocity)
 
 def from_centre(x,y):
     return (centre[0]-x,centre[1]-y)
@@ -35,7 +39,7 @@ def render():
     screen.fill((0, 0, 0))
 
     pygame.draw.circle(screen, (255, 110, 0), centre, 65)
-    pygame.draw.circle(screen, (91, 226, 87), Ship.position, 20)
+    pygame.draw.circle(screen, (91, 226, 87), ship.position, 20)
 
 
 while True:
@@ -45,20 +49,20 @@ while True:
         if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_q]:
             pygame.quit()
         if pygame.key.get_pressed()[pygame.K_a]:
-            Ship.velocity = (Ship.velocity[0]-0.2, Ship.velocity[1])
+            ship.velocity = (ship.velocity[0]-0.2, ship.velocity[1])
         if pygame.key.get_pressed()[pygame.K_w]:
-            Ship.velocity = (Ship.velocity[0], Ship.velocity[1]-0.2)
+            ship.velocity = (ship.velocity[0], ship.velocity[1]-0.2)
         if pygame.key.get_pressed()[pygame.K_d]:
-            Ship.velocity = (Ship.velocity[0]+0.2, Ship.velocity[1])
+            ship.velocity = (ship.velocity[0]+0.2, ship.velocity[1])
         if pygame.key.get_pressed()[pygame.K_s]:
-            Ship.velocity = (Ship.velocity[0], Ship.velocity[1]+0.2)
-    #Ship updates
-    distance = from_centre(Ship.position[0], Ship.position[1])
+            ship.velocity = (ship.velocity[0], ship.velocity[1]+0.2)
+    #Body updates
+    distance = from_centre(ship.position[0], ship.position[1])
     print('distance: ', distance)
     gravity = (g*distance[0], g*distance[1])
     print('gravity: ', gravity)
-    Ship.velocity = (Ship.velocity[0] + gravity[0], Ship.velocity[1] + gravity[1])
-    Ship.position = (Ship.position[0] + Ship.velocity[0], Ship.position[1] + Ship.velocity[1])
+    ship.velocity = (ship.velocity[0] + gravity[0], ship.velocity[1] + gravity[1])
+    ship.position = (ship.position[0] + ship.velocity[0], ship.position[1] + ship.velocity[1])
     render()
     pygame.display.update()
     fps.tick(60)
