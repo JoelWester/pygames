3933e026026ff7c759be9aa7c653485a42c86486
Joel <44907762+JoelWester@users.noreply.github.com>
2022-02-04 17:47:17 +0200

    Add files via upload
    SpaceGame added planet type and planet rendering

--- SpaceGame.py
+++ SpaceGame.py
@@ -19,17 +19,21 @@ g = 0.0001
 #Body object
 
 class Body:
-    position =(50.0, 50.0)
     mass = 0.5
     velocity = (0.0, 0.0)
-    def __init__(self, type):
+    def __init__(self, type, position):
         self.type = type
+        self.position = position
 
-ship = Body("Ship")
+#Ship
+ship = Body("ship", (50.0, 50.0))
 
-print('Body details:')
+#Planets
+a = Body("planet", (centre[0]//2, centre[1]//2))
+planets = {a}
+
+print('Ship details:')
 print('pos: ', ship.position)
-print('mass: ', ship.mass)
 print('velocity: ', ship.velocity)
 
 def from_centre(x,y):
@@ -39,6 +43,8 @@ def render():
     screen.fill((0, 0, 0))
 
     pygame.draw.circle(screen, (255, 110, 0), centre, 65)
+    for planet in planets:
+        pygame.draw.circle(screen, (91, 226, 87), planet.position, 20)
     pygame.draw.circle(screen, (91, 226, 87), ship.position, 20)
 
 
@@ -56,13 +62,19 @@ while True:
             ship.velocity = (ship.velocity[0]+0.2, ship.velocity[1])
         if pygame.key.get_pressed()[pygame.K_s]:
             ship.velocity = (ship.velocity[0], ship.velocity[1]+0.2)
-    #Body updates
+    #Ship updates
     distance = from_centre(ship.position[0], ship.position[1])
     print('distance: ', distance)
     gravity = (g*distance[0], g*distance[1])
     print('gravity: ', gravity)
     ship.velocity = (ship.velocity[0] + gravity[0], ship.velocity[1] + gravity[1])
     ship.position = (ship.position[0] + ship.velocity[0], ship.position[1] + ship.velocity[1])
+    #Planet updates
+    for planet in planets:
+        distance = from_centre(planet.position[0], planet.position[1])
+        gravity = (g*distance[0], g*distance[1])
+        planet.velocity = (planet.velocity[0] + gravity[0], planet.velocity[1] + gravity[1])
+        planet.position = (planet.position[0] + planet.velocity[0], planet.position[1] + planet.velocity[1])
     render()
     pygame.display.update()
     fps.tick(60)
